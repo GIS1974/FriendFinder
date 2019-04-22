06bb098889ab96a93150f6a374ff8c6f1d9ea519
Igor Guba <gubaigor@gmail.com>
2019-04-21 21:47:22 -0400

    Work in progress

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+var friendsData = [
+    {
+        name: "John",
+        photo: "https://pbs.twimg.com/profile_images/972872769019850753/YTxFZF2x_400x400.jpg",
+        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "5", "5"]
+    }
+];
+
+// var userDataArray = [];
+
+module.exports = friendsData;
+
+// module.exports = userDataArray;
